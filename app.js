cabda365a48a2972abd64bf5451fa2793c78311e
Rémy <remy.theroux@gmail.com>
2015-03-02 10:04:10 +0100

    1st commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var dashboard = require('dashboard');
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(dashboard.display());
+}).listen(8124, "127.0.0.1");
+
+console.log('Server running at http://127.0.0.1:8124/');
